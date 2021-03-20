7db4988abd5a3186c2da3a31c0bd7426a9809197
Alek Janevski <aleksandar.janevski@north-47.com>
2021-03-20 19:25:05 +0100

    Added shuffle logic

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,10 +6,24 @@
     <div class="float-left w-1/2 h-full mb-3 border-r p-7">
       <form name="add-user" method="post" @submit.prevent="addUser" class="flex flex-col items-start">
         <!-- Add user input starts -->
+        <label for="">Enter players</label>
         <input 
           v-model="userInput"
           type="text" 
-          placeholder="Enter player name"
+          class="relative w-full max-w-md px-3 py-3 text-sm text-gray-700 placeholder-gray-400 bg-white border border-gray-400 rounded outline-none focus:outline-none focus:shadow-outline"
+        />
+
+        <label for="" class="mt-5">How many players per team</label>
+        <input 
+          v-model="howManyPlayers"
+          type="text" 
+          class="relative w-full max-w-md px-3 py-3 text-sm text-gray-700 placeholder-gray-400 bg-white border border-gray-400 rounded outline-none focus:outline-none focus:shadow-outline"
+        />
+
+        <label for="" class="mt-5">How many teams</label>
+        <input 
+          v-model="howManyTeams"
+          type="text" 
           class="relative w-full max-w-md px-3 py-3 text-sm text-gray-700 placeholder-gray-400 bg-white border border-gray-400 rounded outline-none focus:outline-none focus:shadow-outline"
         />
         <!-- Add user input ends -->
@@ -22,7 +36,7 @@
       </form>
 
       <div v-if="users.length" class="flex flex-col items-start justify-start w-full">
-        <h3 class="block text-xl">Players</h3>
+        <h3 class="block text-xl">All Players:</h3>
         <ul class="flex flex-col items-start justify-start">
           <li v-for="(user, id) in users" :key="id" class="m-0">
             {{ user.name }}
@@ -32,15 +46,29 @@
     </div>
     <!-- Add user wrapper ends -->
 
-
-    <div class="flex flex-col items-center justify-center float-left w-1/2">
+    <!-- Teams section starts -->
+    <div v-if="this.drawArr.length === 0" class="flex flex-col items-center justify-center float-left w-1/2">
       <span class="block mb-2 text-xl">Drawed teams</span>
       <span>please press the button to draw teams</span>
-      <button class="float-right px-4 py-2 mt-6 mb-1 mr-1 text-xs font-bold text-white uppercase bg-blue-500 rounded shadow outline-none active:bg-blue-600 hover:shadow-md focus:outline-none" type="submit" style="transition: all .15s ease">
+      <button @click="shuffle(users)" class="float-right px-4 py-2 mt-6 mb-1 mr-1 text-xs font-bold text-white uppercase bg-blue-500 rounded shadow outline-none active:bg-blue-600 hover:shadow-md focus:outline-none" type="submit" style="transition: all .15s ease">
         Draw teams
       </button>
     </div>
+    <div v-else class="flex flex-col items-center justify-center float-left w-1/2">
+      <ul v-for="(group, i) in articleGroups" :key="i">
+        <label class="font-bold"> Team {{ i + 1 }}: </label>
+        <li v-for="player in drawArr.slice(i * howManyPlayers, (i + 1) * howManyPlayers)" :key="player.id">
+          {{ player.name }}
+        </li>
+      </ul>
+      <span v-if="this.howManyTeams === null">Please select how many teams you want to draw</span>
+      <button @click="shuffle(users)" class="float-right px-4 py-2 mt-6 mb-1 mr-1 text-xs font-bold text-white uppercase bg-blue-500 rounded shadow outline-none active:bg-blue-600 hover:shadow-md focus:outline-none" type="submit" style="transition: all .15s ease">
+        Draw again
+      </button>
+    </div>
+    <!-- Teams section ends -->
 
+    <span v-if="this.totalPlayers > this.drawArr.length"> WAAAAA </span>
   </div>
 </template>
 
@@ -53,22 +81,57 @@ export default {
       users: [
         { id: 1, name: 'John Wick' },
         { id: 2, name: 'John Doe' },
-        { id: 3, name: 'Steve O' }
+        { id: 3, name: 'Steve O' },
+        { id: 4, name: 'Johhny Knoxville' },
+        { id: 5, name: 'Randy B' },
+        { id: 6, name: 'Small G' }
       ],
       userInput: '',
-      drawTeam: false
+      drawTeam: false,
+      drawArr: [],
+      howManyPlayers: null,
+      howManyTeams: null,
+      totalPlayers: null
+    }
+  },
+
+  computed: {
+    articleGroups () {
+      return Array.from(Array(Math.floor(this.howManyTeams)).keys())
     }
   },
 
   methods: {
     addUser() {
-        this.users.push({
-          id:this.users.length+1, 
-          name: this.userInput
-      });
+      this.users.push({
+        id:this.users.length+1, 
+        name: this.userInput
+    });
       
       this.userInput = '';
-    }
+    },
+
+    shuffle(array) {
+      let currentIndex = array.length, temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      this.totalPlayers = this.howManyPlayers * this.howManyTeams
+
+      this.$forceUpdate();
+
+      return this.drawArr = array;
+    }    
   }
 }
 </script>
